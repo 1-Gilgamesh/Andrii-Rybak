2151e06cc649f7b108e10d35e2a247bab951a9e7
1-Gilgamesh <devel1shnxrth@gmail.com>
2023-10-16 17:46:23 +0300

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+﻿while True:
+    a = float ( input("Ведіть перше число: ") )
+    b = float ( input("Ведіть друге число: ") )
+    v = input("Ведіть символ необхіжної дії (+,-,*,/): ")
+    if v == '+':
+        print(str(a + b))
+    elif v == '-': 
+        print(str(a - b))
+    elif v == '*':
+        print(str(a * b))
+    elif v == '//': 
+        if b != 0:
+            print(str(a // b))
+        else: 
+            print("Ділення на 0")
+    else:
+        print("Щось пішло не так. Спробуйте знову")    
